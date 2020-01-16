7c63d4f0a254561ebcd2a995a30d0148fa78c949
Rogerio Fialho <rogeriofialho3d@gmail.com>
2020-01-16 17:07:54 -0300

    create swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Portinari')
+    .setDescription('Portinari Samples API')
+    .setVersion('1.0')
+    .addTag('samples')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
