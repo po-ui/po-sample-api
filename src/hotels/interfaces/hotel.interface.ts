5c6646d05a032bc3675c32c0780680760ae078c3
Gustavo Gomes Madureira <gustavo.gomes@tolentinos.com>
2020-12-17 13:13:22 -0300

    feat(hotels): adiciona endpoint
    Adiciona endpoint hotels que será utilizado no sample po-combo.
    
    Fixes DTHFUI-2671

---
+++ src/hotels/interfaces/hotel.interface.ts
@@ -0,0 +1,19 @@
+/**
+ * HotelS interface
+ */
+export interface Hotel {
+  value: number;
+  id: number;
+  name: string;
+  cnpj: string;
+  email: string;
+  floors: string;
+  category: string;
+  phone: string;
+  note: string;
+  address_zip: string;
+  address_street: string;
+  address_number: string;
+  address_district: string;
+  address_city: string;
+}
