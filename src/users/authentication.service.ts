2d1cdcdbb3753e010ab4e9107bad375c5bc10d78
jcorrea97 <jroberto.cr1@gmail.com>
2021-07-27 12:26:32 -0300

    feat(authentication): adiciona rota de autenticacao
    adiciona rota de autenticao para ser usada no sample Po Page Login - Automatic Service
    DTHFUI-2668

--- src/users/authentication.service.ts
+++ src/users/authentication.service.ts
@@ -1,10 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
 
+import {
+  USER_AUTHENTICATE as userLogin,
+  EMAIL_ERROR as emailError,
+  USER_LOGGED_SUCCESS as loggedSuccess,
+  PASSWORD_ERROR as passwordError
+} from './password-recovery.data';
+
+import * as atob from 'atob'
 @Injectable()
 export class AuthenticationService {
 
   authentication(data) {
+    const result = this.authenticate(data);
 
+    return Promise.resolve(result);
   }
+
+  private authenticate(data) {
+    if (userLogin.login !== data.login) {
+      throw new HttpException(emailError, HttpStatus.NOT_FOUND);
+    }
+    const password = atob(data.password)
+    if (userLogin.password !== password) {
+      throw new HttpException(passwordError, HttpStatus.NOT_FOUND);
+    }
+
+    return loggedSuccess;
+  }
+
 
 }
